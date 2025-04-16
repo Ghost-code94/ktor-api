63394a9469e7bbcc83211ea9c1148f77d1bd7739
abdulwasii adewale <ghost@abdulwasiis-MacBook-Pro.local>
2025-04-16 11:34:44 -0500

    modified the main class to use ktor version 3.x naming conventions

--- src/main/kotlin/ghostcache/api/AppMain.kt
+++ src/main/kotlin/ghostcache/api/AppMain.kt
@@ -1,12 +1,13 @@
 package ghostcache.api
 
-import io.ktor.application.*
-import io.ktor.features.*
-import io.ktor.http.*
-import io.ktor.response.*
-import io.ktor.routing.*
+import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.server.plugins.defaultheaders.*
+import io.ktor.server.plugins.callloging.*
+import io.ktor.server.routing.*
+import io.ktor.server.response.*
+import io.ktor.http.*
 
 fun main() {
     // Use dynamic port (for example, when run in a containerized environment)
